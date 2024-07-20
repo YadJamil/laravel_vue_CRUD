276a0732b68f062c4cb2c03249f525431e5118bd
YadJamil <yadjamil.fcb@gmail.com>
2024-07-21 00:06:38 +0300

    Vue Route Names and Separate File

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,9 @@
 import './bootstrap';
 
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import PostsIndex from './components/Posts/Index.vue'
 import App from './layouts/App.vue'
+import router from './routes/index' 
 
-const routes = [
-    {
-        path: '/',
-        component: PostsIndex
-    },
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
 
 createApp(App) 
     .use(router)
