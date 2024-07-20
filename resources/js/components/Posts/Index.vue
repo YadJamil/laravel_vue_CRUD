4849aad15e2394435bdd384eb9c7a2e8db5cf956
YadJamil <yadjamil.fcb@gmail.com>
2024-07-20 22:16:41 +0300

    Category Dropdown: Second Composable

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
         <div class="min-w-full align-middle">
+
+            <div class="mb-4"> 
+                <select v-model="selectedCategory" class="block mt-1 w-full sm:w-1/4 text-black rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                    <option value="" selected>-- Filter by category --</option>
+                    <option v-for="category in categories" :value="category.id" :key="category.id">
+                        {{ category.name }}
+                    </option>
+                </select>
+            </div> 
+
             <table class="min-w-full divide-y divide-gray-200 border">
                 <thead>
                     <tr>
@@ -80,9 +90,12 @@
 import { onMounted } from "vue";
 import { TailwindPagination } from "laravel-vue-pagination";
 import usePosts from "@/composables/posts";
+import useCategories from "@/composables/categories";
 
 const { posts, getPosts } = usePosts();
+const { categories, getCategories } = useCategories();
 onMounted(() => {
     getPosts();
+    getCategories();
 });
 </script>
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\Api\CategoryController;
 
 
 Route::get('posts', [PostController::class, 'index']);
+Route::get('categories', [CategoryController::class, 'index']);
