dd6117df305e5ad9779dd249c11abaac1ce7dfe9
YadJamil <yadjamil.fcb@gmail.com>
2024-07-21 09:33:00 +0300

    Processing Form: Loading Indicator

--- resources/js/components/Posts/Create.vue
+++ resources/js/components/Posts/Create.vue
@@ -14,11 +14,11 @@
                 type="text"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
             />
-            <div class="text-red-600 mt-1"> 
+            <div class="text-red-600 mt-1">
                 <div v-for="message in validationErrors?.title">
                     {{ message }}
                 </div>
-            </div> 
+            </div>
         </div>
 
         <!-- Content -->
@@ -34,11 +34,11 @@
                 id="post-content"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
             ></textarea>
-            <div class="text-red-600 mt-1"> 
+            <div class="text-red-600 mt-1">
                 <div v-for="message in validationErrors?.content">
                     {{ message }}
                 </div>
-            </div> 
+            </div>
         </div>
 
         <!-- Category -->
@@ -59,19 +59,25 @@
                     {{ category.name }}
                 </option>
             </select>
-            <div class="text-red-600 mt-1"> 
+            <div class="text-red-600 mt-1">
                 <div v-for="message in validationErrors?.category_id">
                     {{ message }}
                 </div>
-            </div> 
+            </div>
         </div>
 
         <!-- Buttons -->
         <div class="mt-4">
             <button
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm uppercase text-white"
+                :disabled="isLoading"
+                class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm uppercase text-white disabled:opacity-75 disabled:cursor-not-allowed"
             >
-                Save
+                <span
+                    v-show="isLoading"
+                    class="inline-block animate-spin w-4 h-4 mr-2 border-t-2 border-t-white border-r-2 border-r-white border-b-2 border-b-white border-l-2 border-l-blue-600 rounded-full"
+                ></span>
+                <span v-if="isLoading">Processing...</span>
+                <span v-else>Save</span>
             </button>
         </div>
     </form>
@@ -83,18 +89,15 @@ import useCategories from "@/composables/categories";
 import usePosts from "@/composables/posts";
 
 const { categories, getCategories } = useCategories();
-const { storePost, validationErrors } = usePosts();
+const { storePost, validationErrors, isLoading } = usePosts();
 
 const post = reactive({
-    title: '',
-    content: '',
-    category_id: '',
+    title: "",
+    content: "",
+    category_id: "",
 });
-
 
 onMounted(() => {
     getCategories();
 });
-
-
 </script>
