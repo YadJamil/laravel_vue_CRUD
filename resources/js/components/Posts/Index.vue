08469c2f447f6b195d8365e0b6b80104602a20ff
YadJamil <yadjamil.fcb@gmail.com>
2024-07-22 15:48:44 +0300

    Extra Table Filters: Column and Global

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -1,25 +1,35 @@
 <template>
     <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
         <div class="min-w-full align-middle">
-            <div class="mb-4">
-                <select
-                    v-model="selectedCategory"
-                    class="block mt-1 w-full sm:w-1/4 text-black rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                >
-                    <option value="" selected>-- Filter by category --</option>
-                    <option
-                        v-for="category in categories"
-                        :value="category.id"
-                        :key="category.id"
-                    >
-                        {{ category.name }}
-                    </option>
-                </select>
-            </div>
+            <div class="mb-4 grid lg:grid-cols-4"> 
+                <input v-model="search_global" type="text" placeholder="Search..." class="inline-block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+            </div> 
 
             <table class="min-w-full divide-y divide-gray-200 border">
                 <thead>
-                    <tr>
+                    <tr> 
+                        <th class="px-6 py-3 bg-gray-50 text-left">
+                            <input v-model="search_id" type="text" class="inline-block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Filter by ID">
+                        </th>
+                        <th class="px-6 py-3 bg-gray-50 text-left">
+                            <input v-model="search_title" type="text" class="inline-block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Filter by Title">
+                        </th>
+                        <th class="px-6 py-3 bg-gray-50 text-left">
+                            <select v-model="search_category" class="inline-block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <option value="" selected>-- all categories --</option>
+                                <option v-for="category in categories" :value="category.id">
+                                    {{ category.name }}
+                                </option>
+                            </select>
+                        </th>
+                        <th class="px-6 py-3 bg-gray-50 text-left">
+                            <input v-model="search_content" type="text" class="inline-block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Filter by Content">
+                        </th>
+                        <th class="px-6 py-3 bg-gray-50 text-left"></th>
+                        <th class="px-6 py-3 bg-gray-50 text-left"></th>
+                    </tr> 
+
+                    <tr>     
                         <th class="px-6 py-3 bg-gray-50 text-left">
                             <div
                                 class="flex flex-row items-center justify-between cursor-pointer"
@@ -165,6 +175,8 @@
                             >
                         </th>
                     </tr>
+
+
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200 divide-solid">
                     <tr v-for="post in posts.data" :key="post.id">
@@ -202,19 +214,14 @@
                                     name: 'posts.edit',
                                     params: { id: post.id },
                                 }"
-                                >Edit</router-link
+                                class="rounded-lg px-3 py-2 bg-yellow-500 text-white">Edit</router-link
                             >
+                            <a href="#" @click.prevent="deletePost(post.id)" class="ml-1 rounded-lg bg-red-400 text-white px-3 py-2">Delete</a> 
                         </td>
                     </tr>
                 </tbody>
             </table>
-            <TailwindPagination
-                :data="posts"
-                @pagination-change-page="
-                    (page) => getPosts(page, selectedCategory)
-                "
-                class="mt-4"
-            />
+            <TailwindPagination :data="posts" @pagination-change-page="page => getPosts(page, search_category)" class="mt-4" /> 
         </div>
     </div>
 </template>
@@ -225,19 +232,27 @@ import { TailwindPagination } from "laravel-vue-pagination";
 import usePosts from "@/composables/posts";
 import useCategories from "@/composables/categories";
 
-const { posts, getPosts } = usePosts();
+const { posts, getPosts, deletePost } = usePosts();
 const { categories, getCategories } = useCategories();
 
-const selectedCategory = ref("");
-const orderColumn = ref("created_at");
-const orderDirection = ref("desc");
+const search_category = ref('') 
+const search_id = ref('')
+const search_title = ref('')
+const search_content = ref('')
+const search_global = ref('') 
+const selectedCategory = ref('')
+const orderColumn = ref("created_at")
+const orderDirection = ref("desc")
 
 const updateOrdering = (column) => {
     orderColumn.value = column;
     orderDirection.value = orderDirection.value === "asc" ? "desc" : "asc";
     getPosts(
         1,
-        selectedCategory.value,
+        search_category.value, 
+        search_id.value,
+        search_title.value,
+        search_content.value, 
         orderColumn.value,
         orderDirection.value
     );
@@ -248,8 +263,57 @@ onMounted(() => {
     getCategories();
 });
 
-watch(selectedCategory, (current, previous) => {
-    getPosts(1, current);
-});
+watch(search_category, (current, previous) => { 
+    getPosts(
+        1,
+        current,
+        search_id.value,
+        search_title.value,
+        search_content.value,
+        search_global.value
+    )
+}) 
+
+watch(search_id, (current, previous) => { 
+    getPosts(
+        1,
+        search_category.value,
+        current,
+        search_title.value,
+        search_content.value,
+        search_global.value
+    )
+})
+watch(search_title, (current, previous) => {
+    getPosts(
+        1,
+        search_category.value,
+        search_id.value,
+        current,
+        search_content.value,
+        search_global.value
+    )
+})
+watch(search_content, (current, previous) => {
+    getPosts(
+        1,
+        search_category.value,
+        search_id.value,
+        search_title.value,
+        current,
+        search_global.value
+    )
+}) 
+
+watch(search_global, (current, previous) => { 
+    getPosts(
+        1,
+        search_category.value,
+        search_id.value,
+        search_title.value,
+        search_content.value,
+        current
+    )
+}) 
 
 </script>
--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -3,20 +3,28 @@ import { useRouter } from 'vue-router'
 
 export default function usePosts() {
     const posts = ref({})
-    const router = useRouter() 
-    const validationErrors = ref({}) 
-    const isLoading = ref(false) 
-    const post = ref({}) 
+    const router = useRouter()
+    const validationErrors = ref({})
+    const isLoading = ref(false)
+    const post = ref({})
     const swal = inject('$swal')
 
     const getPosts = async (
         page = 1,
-        category = '',
+        search_category = '', 
+        search_id = '',
+        search_title = '',
+        search_content = '', 
+        search_global = '', 
         order_column = 'created_at',
         order_direction = 'desc'
     ) => {
         axios.get('/api/posts?page=' + page +
-            '&category=' + category +
+            '&search_category=' + search_category + 
+            '&search_id=' + search_id +
+            '&search_title=' + search_title +
+            '&search_content=' + search_content + 
+            '&search_global=' + search_global + 
             '&order_column=' + order_column +
             '&order_direction=' + order_direction)
             .then(response => {
@@ -24,55 +32,55 @@ export default function usePosts() {
             })
     }
 
-    const storePost = async (post) => { 
-        if (isLoading.value) return; 
- 
-        isLoading.value = true 
-        validationErrors.value = {} 
+    const storePost = async (post) => {
+        if (isLoading.value) return;
 
-        let serializedPost = new FormData() 
+        isLoading.value = true
+        validationErrors.value = {}
+
+        let serializedPost = new FormData()
         for (let item in post) {
             if (post.hasOwnProperty(item)) {
                 serializedPost.append(item, post[item])
             }
         }
- 
-        axios.post('/api/posts', serializedPost) 
+
+        axios.post('/api/posts', serializedPost)
             .then(response => {
                 router.push({ name: 'posts.index' })
-                swal({ 
+                swal({
                     icon: 'success',
                     title: 'Post saved successfully'
-                }) 
+                })
             })
-            .catch(error => { 
+            .catch(error => {
                 if (error.response?.data) {
                     validationErrors.value = error.response.data.errors
-                    isLoading.value = false 
+                    isLoading.value = false
                 }
-            }) 
-    } 
+            })
+    }
 
-    const getPost = async (id) => { 
+    const getPost = async (id) => {
         axios.get('/api/posts/' + id)
             .then(response => {
                 post.value = response.data.data;
             })
-    } 
+    }
 
-    const updatePost = async (post) => { 
+    const updatePost = async (post) => {
         if (isLoading.value) return;
- 
+
         isLoading.value = true
         validationErrors.value = {}
- 
+
         axios.put('/api/posts/' + post.id, post)
             .then(response => {
                 router.push({ name: 'posts.index' })
-                swal({ 
+                swal({
                     icon: 'success',
                     title: 'Post updated successfully'
-                }) 
+                })
             })
             .catch(error => {
                 if (error.response?.data) {
@@ -82,6 +90,53 @@ export default function usePosts() {
             .finally(() => isLoading.value = false)
     } 
 
-    return { posts, getPosts, storePost, validationErrors, isLoading, getPost, post, updatePost } 
+    const deletePost = async (id) => {
+        swal({
+            title: 'Are you sure?',
+            text: 'You won\'t be able to revert this action!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            confirmButtonColor: '#ef4444',
+            timer: 20000,
+            timerProgressBar: true,
+            reverseButtons: true
+        })
+            .then(result => {
+                if (result.isConfirmed) {
+                    axios.delete('/api/posts/' + id)
+                        .then(response => {
+                            getPosts()
+                            router.push({ name: 'posts.index' })
+                            swal({
+                                icon: 'success',
+                                title: 'Post deleted successfully'
+                            })
+                        })
+                        .catch(error => {
+                            swal({
+                                icon: 'error',
+                                title: 'Something went wrong'
+                            })
+                        })
+                }
+            })
+
+
+
+
+    }
+
+    return {
+        posts,
+        getPosts,
+        post,
+        getPost,
+        storePost,
+        updatePost,
+        deletePost,
+        validationErrors,
+        isLoading,
+    }
 }
 
